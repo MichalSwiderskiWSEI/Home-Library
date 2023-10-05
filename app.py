6f6f01e80896d53b71ff21370757bc75cd4c09c2
Michal Swiderski <michal.swiderski@wseiz.edu.pl>
2023-10-05 17:10:17 +0200

    'logon-page'

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+
+#Custom Error Pages
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
